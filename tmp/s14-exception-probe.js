84da1dbc739d722af929fcdd214ca4a00eab4396
Rob Lourens <roblourens@gmail.com>
2026-05-12 14:48:22 -0700

    Cleanup

--- tmp/s14-exception-probe.js
+++
@@ -1,7 +0,0 @@
-process.on('uncaughtException', error => {
-  console.error('[probe uncaughtException]', error.message);
-});
-
-setInterval(() => {
-  // Keep the process alive for debugger interaction.
-}, 1000);
